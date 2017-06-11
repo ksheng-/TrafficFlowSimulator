2754fdffb52af1add68a08ab0e588923b2bf8454
ksheng- <ksheng7746@gmail.com>
2017-06-11 03:06:03 -0400

    added pigou

--- simulator/optimizer.py
+++ simulator/optimizer.py
@@ -1,17 +1,16 @@
 import cvxpy as cvx
 import numpy as np
 
-if __name__ == '__main__':
-    import netlist as nl
-    properties = nl.load('example')
-    
+def optimize(properties):
+    print(properties['graph'])
     df = properties['graph'].set_index(['node 1', 'node 2'])
-    print(df)
+    
+    print(df.index.values)
    
     constraints = []
     drivers = [[cvx.Variable() for j in properties['routes'][i]] for i in properties['routes']]
     for i, trip in enumerate(properties['routes']):
-        constraints.append(sum(drivers[i]) == properties['agents'][i])
+        constraints.append(sum(drivers[i]) = properties['agents'][i])
     
     
     for i, trip in enumerate(properties['routes']):
@@ -19,23 +18,30 @@ if __name__ == '__main__':
             print(route)
                            
                 
-    print(sum([df.loc[route[n], route[n+1]]['fft'].iloc[0]
-         + 100 * df.loc[route[n], route[n+1]]['ddt'].iloc[0] 
+    print(sum([df.loc[route[n], route[n+1]]['fft']
+         + 100 * df.loc[route[n], route[n+1]]['ddt']
             for i, trip in enumerate(properties['routes'])
                 for j, route in enumerate(properties['routes'][trip]) 
-                    for n in range(len(route) - 1)]))
+                    for n in range(len(route) - 1)])/3)
     obj = cvx.Minimize(
-        sum([df.loc[route[n], route[n+1]]['fft'].iloc[0]
-             + drivers[i][j] * df.loc[route[n], route[n+1]]['ddt'].iloc[0] 
+        sum([df.loc[route[n], route[n+1]]['fft']
+             + drivers[i][j] * df.loc[route[n], route[n+1]]['ddt']
                 for i, trip in enumerate(properties['routes'])
                     for j, route in enumerate(properties['routes'][trip]) 
                         for n in range(len(route) - 1)])
     )
 
     prob = cvx.Problem(obj, constraints)
-    prob.solve(solver='SCS', verbose=False)
+    prob.solve(solver='SCS', verbose=True)
     print(prob.status)
     print(prob.value)
     for i, trip in enumerate(drivers):
         for j, route in enumerate(trip):
             print(drivers[i][j].value)
+
+if __name__ == '__main__':
+    import netlist as nl
+    properties = nl.load('pigou_single')
+    
+    optimize(properties)
+
